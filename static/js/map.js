507240eb6739542936918bbb52e05f28d655f5f6
Ke Zhao <kezhao@Kes-MacBook-Pro.local>
2018-03-03 10:46:47 -0600

    change

--- static/js/map.js
+++ static/js/map.js
@@ -24,47 +24,43 @@ var statelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satelli
 // ########################################## 
 // Creating Map Layer and Base Layers
 
-var myMap = L.map("mapid", {
-    center: [
-        37.8, -96
-    ],
-    zoom: 4,
-    layers: [lightMap, statelliteMap]
-});
-
-
-var baseMaps = {
-    "Lightmap": lightMap,
-    "Statellitemap": statelliteMap
-};
 
 
 
 
 d3.json(queryUrl, function(response) {
+  
+
+    
     var markers = L.markerClusterGroup();
 
-    for (var i = 0; i < response.length; i++) {
-
-
-        var heat = L.heatLayer(
-            response[i].latitude, response[i].longitude, 0.2, {
-                radius: 25
-            });
-
-        markers.addLayer(L.marker([response[i].latitude, response[i].longitude])
-            .bindPopup("<p style=`font-size:1.5em`> <b>Case Id:</b> " + response[i].case_id + "</p><p style=`font-size:1.5em`><b>Case Date: </b>" + response[i].date + "</p><p style=`font-size:1.5em`><b>Number of Drunk Drivers Involved:</b> " + response[i].drunk_drivers + "</p><p style=`font-size:1.5em`><b>Number of Fatalities: </b>" + response[i].fatalities + "</p>"));
-    }
-
-    var overlayMaps = {
-        "Accidents": markers,
-//        "HeatMap": heat
-    };
+    
+    
+    
     var baseMaps = {
         "<span style='color: gray'>LightMap</span>": lightMap,
         "Statellitemap": statelliteMap
     };
+    var overlayMaps = {
+        "Accidents": markers
+    };
 
+
+
+  
+
+    for (var i = 0; i < response.length; i++) {
+         markers.addLayer(L.marker([response[i].latitude, response[i].longitude]).bindPopup("<p style=`font-size:1.5em`> <b>Case Id:</b> " + response[i].case_id + "</p><p style=`font-size:1.5em`><b>Case Date: </b>" + response[i].date + "</p><p style=`font-size:1.5em`><b>Number of Drunk Drivers Involved:</b> " + response[i].drunk_drivers + "</p><p style=`font-size:1.5em`><b>Number of Fatalities: </b>" + response[i].fatalities + "</p>"));
+    }
+
+
+    var myMap = L.map("mapid", {
+        center: [
+            37.8, -96
+        ],
+        zoom: 4,
+        layers: [lightMap, statelliteMap,markers]
+    });
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 });
